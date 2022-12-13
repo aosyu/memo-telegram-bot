6ac72a66ee5311eb926cc575d6d2dcd5f649a657
Anna Syuris <ann.syuris@gmail.com>
2022-12-13 22:49:09 +0300

    api error handler updated

--- src/handlers/ApiErrorHandler.php
+++ src/handlers/ApiErrorHandler.php
@@ -2,14 +2,19 @@
 
 namespace App\handlers;
 
+use Psr\SimpleCache\InvalidArgumentException;
 use SergiX44\Nutgram\Nutgram;
 use Throwable;
 
 class ApiErrorHandler
 {
+    /**
+     * @throws InvalidArgumentException
+     */
     public function __invoke(Nutgram $bot, Throwable $exception): void
     {
         $bot->sendMessage('Ошибка ' . $exception->getCode());
+        $bot->endConversation();
         if ($_ENV['MODE'] == 'dev') {
             echo $exception->getMessage();
             echo $exception->getCode();
